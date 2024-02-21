dda075d4f3b37f7fff6d0a9c40e5fc0b531f6c0f
Dieu Vu <vuthimaidieu378460@gmail.com>
2024-02-21 20:54:31 +0700

    update logic to check call in telephone_time dict

--- project1-unscramble-computer-science-problems/Task2.py
+++ project1-unscramble-computer-science-problems/Task2.py
@@ -25,7 +25,7 @@ telephone_time = dict()
 for call in calls:
     # call[0] is the caller (making a call), call[1] is the receiver (answering the call), call[3] is the duration
     for call_element in [0, 1]:
-        if call_element not in telephone_time:
+        if call[call_element] not in telephone_time:
             telephone_time[call[call_element]] = 0
         telephone_time[call[call_element]] += int(call[3])
 
